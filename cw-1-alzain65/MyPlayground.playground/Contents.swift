43efb0b561e4ab2dd65c9bc2e1464a9d3f3f5ee5
alzainalbannai <alzainalbannai@users.noreply.github.com>
2020-06-13 21:23:25 +0300

    done cw 1 & 2
    easy

--- cw-1-alzain65/MyPlayground.playground/Contents.swift
+++ cw-1-alzain65/MyPlayground.playground/Contents.swift
@@ -12,6 +12,35 @@ var age = 17
 
 var height = 161
 
-print ( name, hobby1, hobby2, age, height)
+print ( "my name is:", name, "my hobbies are:",  hobby1,"-", hobby2,"my age is:", age,"my height", height)
 
 
+let friendName = " batool"
+
+let freindHieght = 169
+
+let friendAge = 17
+
+let friendHobby1 = "الرسم"
+
+let friendHobby2 = "gymnastics"
+
+var age_difference = age - friendAge
+
+name != friendName
+name == friendName
+
+hobby1 != friendHobby1
+hobby1 == friendHobby1
+hobby1 != friendHobby2
+hobby2 != friendHobby2
+hobby2 == friendHobby2
+hobby2 != friendHobby1
+height != freindHieght
+height == freindHieght
+height > freindHieght
+height >= freindHieght
+height < freindHieght
+height <= freindHieght
+
+let bestFriends = hobby1 == friendHobby1 || hobby1 == friendHobby2 || hobby2 == friendHobby1 || hobby2 == friendHobby2 || height == freindHieght || age == friendAge || name == friendName 
