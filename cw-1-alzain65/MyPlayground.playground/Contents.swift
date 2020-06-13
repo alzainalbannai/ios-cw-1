b6822e55c3a01d76e34fe2bf931ac3e1b8ea82fc
alzainalbannai <alzainalbannai@users.noreply.github.com>
2020-06-13 19:10:18 +0300

    done
    done completely

---
+++ cw-1-alzain65/MyPlayground.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+var str = "Hello, playground"
+
+let name = "alzain"
+
+var hobby1 = "gymnastics"
+
+var hobby2 = "volleyball"
+
+var age = 17
+
+var height = 161
+
+print ( name, hobby1, hobby2, age, height)
+
+
